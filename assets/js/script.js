8b5c3343d9fd0f1f0138570c07e33a3b7934da79
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-01-29 01:51:20 -0500

    corrected file paths

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,65 @@
-//timer
+// variables needed
+var timer = 75
+var score = ''
+
+
+
+//array with questions
+var quizQuestions = [
+  {
+    question: "An if/else statement is enclosed with ______________.",
+    possibleAnswers: ["Parentheses", "Curly Brackets", "Square Brackets", "Quotations"],
+    correctAnswer: "A" 
+  },
+  {
+    question: "",
+    possibleAnswers: ["", "", "", ""],
+    correctAnswer: "C" 
+  },
+  {
+    question: "",
+    possibleAnswers: ["", "", "", ""],
+    correctAnswer: "B" 
+  },
+  {
+    question: "",
+    possibleAnswers: ["", "", "", ""],
+    correctAnswer: "D" 
+  },
+  {
+    question: "",
+    possibleAnswers: ["", "", "", ""],
+    correctAnswer: "A" 
+  },
+];
+
+//game ends when timer ends or end of questions
+var endGame = function() {
+  //enter high score
+
+  //save in localstorage
+  
+  //display current high scores
+};
+
+var showHighScore = function() {
+  //load from local storage
+  //display on highscore.html page
+};
 
 //present and loop through questions
+var answerQuestion = function() {
+  if 
+  //answer correct, move to next question
 
-//deduct from timer on wrong answer
+  //answer incorrect, deduct points and move to next question
+}
 
-//all questions answered or timer = 0, game over
+//start game and timer on button click
+var startGame = function() {
+  //Show start quiz page
+  //start timer on click
+  //show first question and create loop in answerQuestion fucntion
+};
 
-//save high score and have it persist
+startGame();
